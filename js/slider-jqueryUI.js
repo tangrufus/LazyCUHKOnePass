71ebabc6fd623d70b0496473bb5ae372250e95b2
Tang Rufus <tangrufus@gmail.com>
2013-09-20 00:22:03 +0800

    min: 10  --> 20

--- js/slider-jqueryUI.js
+++ js/slider-jqueryUI.js
@@ -2,7 +2,7 @@ $(function() {
 	$( "#slider-range-min" ).slider({
 		range: "min",
 		value: 50,
-		min: 10,
+		min: 20,
 		max: 500,
 		slide: function( event, ui ) {
 			$( "#amount" ).val( "HKD $" + ui.value );
