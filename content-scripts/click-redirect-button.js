40ace17609a504dc4ae0099684fa3116207a5ded
Tang Rufus <tangrufus@gmail.com>
2017-11-06 20:47:26 +0800

    Use eslint

--- content-scripts/click-redirect-button.js
+++ content-scripts/click-redirect-button.js
@@ -1,15 +1,15 @@
 function onRedirectPage() {
-  const pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
-  return pageHTML.indexOf("This will log you in via the CUHK Central Authentication System") > 0 ||
-    pageHTML.indexOf("經中大認證系統登入");
+  const pageHTML = (document.getElementsByTagName('html')[0].innerHTML);
+  return pageHTML.indexOf('This will log you in via the CUHK Central Authentication System') > 0 ||
+    pageHTML.indexOf('經中大認證系統登入');
 }
 
 function hasRedirectButton() {
-  return document.querySelector("a[href^=\"./?languageCd=\"]") !== null;
+  return document.querySelector('a[href^="./?languageCd="]') !== null;
 }
 
 function clickRedirectButton() {
-  document.querySelector("a[href^=\"./?languageCd=\"]").click();
+  document.querySelector('a[href^="./?languageCd="]').click();
 }
 
 if (onRedirectPage() && hasRedirectButton()) {
--- content-scripts/submit-login-form.js
+++ content-scripts/submit-login-form.js
@@ -1,36 +1,36 @@
 function onLoginPage() {
-  const pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
-  return (pageHTML.indexOf("Welcome to OnePass") > 0 &&
-      pageHTML.indexOf("CADS Reference Number: 246") > 0) ||
-    (pageHTML.indexOf("歡迎使用 OnePass") > 0 &&
-      pageHTML.indexOf("CADS 參考編號: 246") > 0);
+  const pageHTML = (document.getElementsByTagName('html')[0].innerHTML);
+  return (pageHTML.indexOf('Welcome to OnePass') > 0 &&
+      pageHTML.indexOf('CADS Reference Number: 246') > 0) ||
+    (pageHTML.indexOf('歡迎使用 OnePass') > 0 &&
+      pageHTML.indexOf('CADS 參考編號: 246') > 0);
 }
 
 function hasLoginForm() {
-  const pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
-  return pageHTML.indexOf("<form name=\"loginForm\" ") > 0
+  const pageHTML = (document.getElementsByTagName('html')[0].innerHTML);
+  return pageHTML.indexOf('<form name="loginForm" ') > 0;
 }
 
 function hasFormError() {
-  const pageHTML = (document.getElementsByTagName("html")[0].innerHTML);
-  return (pageHTML.indexOf("Incorrect Login ID or Password") > 0 ||
-    pageHTML.indexOf("密碼不正確") > 0);
+  const pageHTML = (document.getElementsByTagName('html')[0].innerHTML);
+  return (pageHTML.indexOf('Incorrect Login ID or Password') > 0 ||
+    pageHTML.indexOf('密碼不正確') > 0);
+}
+
+function submitLoginForm(username, password) {
+  document.getElementsByName('username')[0].value = username;
+  document.getElementsByName('password')[0].value = password;
+  document.getElementsByName('loginForm')[0].submit();
 }
 
 function checkCredentialsAndSubmitLoginForm() {
-  chrome.storage.local.get(["username", "password"], function(credentials) {
+  chrome.storage.local.get(['username', 'password'], (credentials) => {
     if (!credentials.hasOwnProperty('username') || !credentials.hasOwnProperty('password')) {
       return;
     }
 
-    submitLoginForm(credentials.username, credentials.password)
+    submitLoginForm(credentials.username, credentials.password);
   });
-}
-
-function submitLoginForm(username, password) {
-  document.getElementsByName("username")[0].value = username;
-  document.getElementsByName("password")[0].value = password;
-  document.getElementsByName("loginForm")[0].submit();
 }
 
 if (onLoginPage() && hasLoginForm() && !hasFormError()) {
